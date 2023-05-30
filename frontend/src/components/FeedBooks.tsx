997cfc6a4160a982374313f8f85eadd85fe2037e
Jenniina <jenniina@jenniina.fi>
2023-05-30 16:20:38 +0300

    remove unused var

--- frontend/src/components/FeedBooks.tsx
+++ frontend/src/components/FeedBooks.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { booksProps } from '../interfaces'
-import { ApolloQueryResult } from '@apollo/client'
 import { Link } from 'react-router-dom'
 
 interface Props {
