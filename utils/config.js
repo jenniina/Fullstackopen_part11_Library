9d20c30d9bccabbea349f949f97206e4848184c0
Jenniina <jenniina@jenniina.fi>
2023-05-31 16:09:14 +0300

    change MONGODB_URI

--- utils/config.js
+++ utils/config.js
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
 
-// const MONGODB_URI =
-//   process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
-
 const MONGODB_URI =
-  process.env.NODE_ENV === 'test' ? 'mongodb://127.0.0.1:27017' : process.env.MONGODB_URI
+  process.env.NODE_ENV === 'test' ? process.env.TEST_MONGODB_URI : process.env.MONGODB_URI
+
+// const MONGODB_URI =
+//   process.env.NODE_ENV === 'test' ? 'mongodb://127.0.0.1:27017' : process.env.MONGODB_URI
 
 const BUILD =
   process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
